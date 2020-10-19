718df2c5c0352bf48a2634a82386cad44d016c58
Ruslan <rsivitsky@gmail.com>
2020-10-20 01:27:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const box = document.getElementById("box");
+console.log(box);
+
+const btns = document.getElementsByTagName("button");
+//console.log(btns);
+btns[1].style.backgroundColor = "blue";
